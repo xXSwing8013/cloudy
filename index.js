b686abc94a60cf3aa445d7d39004c0b85e88a675
xXSwing8013 <65372262+xXSwing8013@users.noreply.github.com>
2020-07-06 22:10:05 -0700

    index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+CODE TO START BOT 
+const Discord = require('discord.js');
+const Client = new Discord.Client();
+
+const token = '(token)';
+
+client.on('ready', () =>{
+    console.log('This bot is online');
+})
+
+client.login(token)
+COMMANDS CODE
